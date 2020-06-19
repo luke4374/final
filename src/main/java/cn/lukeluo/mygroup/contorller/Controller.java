d3c85b94d571d842736c3fed376104575645c230
luke4374 <luke31312@sina.com>
2020-06-19 22:04:58 +0800

    ccc

--- src/main/java/cn/lukeluo/mygroup/contorller/Controller.java
+++ src/main/java/cn/lukeluo/mygroup/contorller/Controller.java
@@ -13,7 +13,7 @@ public class Controller {
     @RequestMapping("/albums")
     public List<Album> findAllAlbums(){
     List<Album> albums = new ArrayList<>();
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 5; i++) {
          Album a=new Album();
          a.setId(i);
          a.setName("album"+i);
